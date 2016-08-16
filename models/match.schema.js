e65b007ac278e356a2780e389e1c2d47221c88e6
cchia <casperchiakyechuen@gmail.com>
2016-08-17 09:46:27 +1000

    Update match schema to use player name as id instead of playerId

--- models/match.schema.js
+++ models/match.schema.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var matchSchema = mongoose.Schema({
-   player1: {playerId: String, score: Number},
-   player2: {playerId: String, score: Number},
+   player1: {name: String, score: Number},
+   player2: {name: String, score: Number},
    datePlayed: {type: String, required: true},
    winner: String,
    mode: {type: String, required: true}
